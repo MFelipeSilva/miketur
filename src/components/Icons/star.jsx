0b85707fd942019f4f873597f78a9e9ab77e6ca0
Felipe da Silva <silvaafelipe016@gmail.com>
2024-07-15 15:24:42 -0300

    feat(icons): added more icons in component

---
+++ src/components/Icons/star.jsx
@@ -0,0 +1,13 @@
+export const IconStar = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="#ffcb0c"
+      d="m7.325 18.923l1.24-5.313l-4.123-3.572l5.431-.47L12 4.557l2.127 5.01l5.43.47l-4.123 3.572l1.241 5.313L12 16.102z"
+    />
+  </svg>
+);
---
+++ src/components/Icons/versus.jsx
@@ -0,0 +1,13 @@
+export const IconVersus = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="45"
+    height="45"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M24 2.4L21.6 0L12 9.6L2.4 0L0 2.4L9.6 12L0 21.6L2.4 24l9.6-9.6l9.6 9.6l2.4-2.4l-9.6-9.6z"
+    />
+  </svg>
+);
