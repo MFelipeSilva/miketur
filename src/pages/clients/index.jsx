4de61b137a2826702b47c1f3169ce639fd0eb0cd
Felipe da Silva <silvaafelipe016@gmail.com>
2024-07-15 15:29:00 -0300

    feat(pages): updates to the clients page

--- src/pages/clients/index.jsx
+++ src/pages/clients/index.jsx
@@ -1,21 +1,32 @@
-import styles from "./Clients.module.css";
-
 import cloud_01 from "../../assets/cloud-01.webp";
 import cloud_02 from "../../assets/cloud-02.webp";
+
+import { clients } from "../../utils/clients";
+
+import { Stars } from "../../components/Stars";
+
+import styles from "./Clients.module.css";
 
 function Clients() {
   return (
     <section className={styles.section_container}>
-      <img src={cloud_01} className={styles.cloud_01} alt="" />
+      <img src={cloud_01} className={styles.cloud_01} alt="Nuvem 1" />
       <div className={styles.content}>
         <h2>Opiniões dos nossos clientes:</h2>
         <div className={styles.card_container}>
-          <div className={styles.clients_card} />
-          <div className={styles.clients_card} />
-          <div className={styles.clients_card} />
+          {clients.map((client) => (
+            <div className={styles.clients_card} key={client.id}>
+              <div>
+                <img src={client.image} alt="avatar dos clientes" />
+                <h3>{client.name}</h3>
+                <Stars />
+              </div>
+              <p>{client.description}</p>
+            </div>
+          ))}
         </div>
       </div>
-      <img src={cloud_02} className={styles.cloud_02} alt="" />
+      <img src={cloud_02} className={styles.cloud_02} alt="Nuvem 2" />
     </section>
   );
 }
---
+++ src/utils/clients.js
@@ -0,0 +1,30 @@
+import name_avatar_01 from "../assets/name-avatar-01.png";
+import name_avatar_02 from "../assets/name-avatar-02.png";
+import name_avatar_03 from "../assets/name-avatar-03.png";
+
+export const clients = [
+  {
+    id: "1",
+    image: name_avatar_01,
+    name: "Gislene M.",
+    stars: 5,
+    description:
+      "Ótima agência de viagens! O pessoal é super atencioso e os preços são os melhores. Minha viagem foi incrível e ocorreu super bem. Vou planejar a próxima com eles, com certeza! Recomendo muito!",
+  },
+  {
+    id: "2",
+    image: name_avatar_02,
+    name: "Tiago N.",
+    stars: 5,
+    description:
+      "Com certeza uma das melhores experiências que já tive em viagens, todo apoio e suporte que a MikeTur prestou facilitou muito e só agregou para que fosse tudo mais incrível!!",
+  },
+  {
+    id: "3",
+    image: name_avatar_03,
+    name: "Paula F.",
+    stars: 5,
+    description:
+      "Estava pesquisando passagens de avião para Porto Seguro em um feriado, todos horários estavam caros, pedi ajuda para o Kaio e ele conseguiu passagens em um ótimo horário e mais baratas. Recomendo a Miketur. Excelente atendimento.",
+  },
+];
