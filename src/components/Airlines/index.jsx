4261ab787b2cd0b3d6e84dfe49e14a63a97f4760
Felipe da Silva <silvaafelipe016@gmail.com>
2024-07-15 21:13:18 -0300

    feat(components): changes in the airlines component

--- src/components/Airlines/index.jsx
+++ src/components/Airlines/index.jsx
@@ -16,6 +16,43 @@ export const Airlines = () => {
     slidesToScroll: 1,
     cssEase: "linear",
     pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2.5,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
