8cebaaabff916cbbe4f1f4ad7e23131ae71c1c63
Felipe da Silva <silvaafelipe016@gmail.com>
2024-07-16 10:32:30 -0300

    fix(files): fixed issues in some files

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,10 +4,7 @@ import styles from "./Button.module.css";
 
 export const PrimaryButton = ({ children, onClick }) => {
   return (
-    <a
-      href="https://wa.me/5517981606207"
-      target="_blank"
-    >
+    <a href="https://wa.me/5517981606207" target="_blank" rel="noreferrer">
       <button onClick={onClick} className={styles.primary_button}>
         <IconWhatsapp />
         {children}
